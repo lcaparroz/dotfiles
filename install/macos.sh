1bd5548ff0ebf1251f0445befd52cad74d8b2361
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-08-11 22:31:58 -0300

    Add instalation files for macOS and karabiner

---
+++ install/macos.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Import functions
+source "${HOME}/.dotfiles/install/functions.sh"
+
+readonly KARABINER_DIR="${HOME}/.config/karabiner"
+readonly KARABINER_MODS_DIR="${KARABINER_DIR}/assets/complex_modifications"
+
+# Karabiner configuration
+create_directory KARABINER_MODS_DIR
+create_symbolic_link \
+  "${HOME}/.dotfiles/macos/karabiner/custom_modifications.json" \
+  "${KARABINER_MODS_DIR}/custom.json"
+
+create_symbolic_link /usr/local/etc/bash_completion.d/git-prompt.sh \
+  "${HOME}/.git-prompt.sh"
+
+create_symbolic_link "${HOME}/.dotfiles/bash/macos.sh" \
+  "${HOME}/.bashrc_os"
