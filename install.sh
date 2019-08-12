15b75e22cad9849c459cb138040f396327ebb932
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-08-12 08:18:40 -0300

    Add Emacs setup to install.sh

--- install.sh
+++ install.sh
@@ -5,20 +5,23 @@ source "${HOME}/.dotfiles/install/functions.sh"
 
 echo -e "# Dotfiles Installation #\n"
 
+create_directory "${HOME}/.emacs.d"
+create_directory "${HOME}/.vim/.undo"
+create_directory "${HOME}/.vim/.backup"
+create_directory "${HOME}/.vim/.swap"
+
 create_symbolic_link "${HOME}/.dotfiles/bash/bash_aliases" \
   "${HOME}/.bash_aliases"
 create_symbolic_link "${HOME}/.dotfiles/bash/bashrc" \
   "${HOME}/.bashrc"
+create_symbolic_link "${HOME}/.dotfiles/emacs/init.el" \
+  "${HOME}/.emacs.d/init.el"
 create_symbolic_link "${HOME}/.dotfiles/tmux/tmux.conf" \
   "${HOME}/.tmux.conf"
 create_symbolic_link "${HOME}/.dotfiles/vim/vimrc" \
   "${HOME}/.vimrc"
 create_symbolic_link "${HOME}/.dotfiles/vim/plugins.vim" \
   "${HOME}/.vim/plugins.vim"
-
-create_directory "${HOME}/.vim/.undo"
-create_directory "${HOME}/.vim/.backup"
-create_directory "${HOME}/.vim/.swap"
 
 source_file_if_linux "${HOME}/.dotfiles/install/linux.sh"
 source_file_if_macos "${HOME}/.dotfiles/install/macos.sh"
