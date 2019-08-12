c4860b31ae8eeec352aa34129cc717af153924b6
Luis Caparroz <lcaparroz.dev@gmail.com>
2019-08-11 22:17:05 -0300

    Extract functions from install.sh and rewrite them

--- install.sh
+++ install.sh
@@ -1,84 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Dotfiles installation
-readonly SUCCESS=0
-readonly FAIL=1
-readonly BACKUP_SUFFIX='.dotfiles.bkp'
-readonly DOTFILES_LOG="${HOME}/.dotfiles.log"
+# Import functions
+source "${HOME}/.dotfiles/install/functions.sh"
 
-function create_symbolic_link() {
-  local target_file=$1
-  local symlink_destination=$2
-
-  local msg_create_backup
-  local msg_create_symlink
-  msg_create_backup='* Create backup file for'
-  msg_create_symlink='* Create symbolic link for'
-
-  echo -e "\nCreating symbolic link for ${target_file}:"
-  if [[ -L "${symlink_destination}" ]] ; then
-    local symlink_target
-    symlink_target=$(readlink "${symlink_destination}")
-    echo ". Found a symbolic link. Target: ${symlink_target}"
-    if [[ "${symlink_target}" = "${target_file}" ]] ; then
-      echo '.. Target is ok (no further action required).'
-      return "${SUCCESS}"
-    else
-      echo '.. Unexpected target. Required actions:'
-      echo "... ${msg_create_backup} ${symlink_destination};"
-      echo "... ${msg_create_symlink} ${target_file}."
-    fi
-  else
-    if [[ -f "${destination_symlink}" ]] ; then
-      echo '. Found a file (not a symbolic link).'
-      echo '.. Required actions:'
-      echo "... ${msg_create_backup} ${symlink_destination};"
-      echo "... ${msg_create_symlink} ${target_file}."
-    else
-      echo '. No file or symbolic link found.'
-      echo '.. Required actions:'
-      echo "... ${msg_create_symlink} ${target_file}."
-    fi
-  fi
-
-  echo -e "\n. Executing actions:"
-  ln --symbolic --backup=existing --suffix="${BACKUP_SUFFIX}" \
-    "${target_file}" "${symlink_destination}" 2>> "${DOTFILES_LOG}"
-  if [[ "$?" -ne 0 ]]; then
-    echo ".. Error on creating symbolic link."
-    echo '. Error on creating symbolic link:' >> "${DOTFILES_LOG}"
-    echo -e ".. ${symlink_destination} -> ${target_file}\n" \
-      >> "${DOTFILES_LOG}"
-    return "${FAIL}"
-  else
-    local backup_file
-    backup_file="${symlink_destination}${BACKUP_SUFFIX}"
-    if [[ -e "${backup_file}" ]] ; then
-      echo ".. Backup in: ${backup_file}"
-    fi
-    if [[ -L "${symlink_destination}" ]] ; then
-      echo ".. Symbolic link in: ${symlink_destination}"
-      echo "... Target in: ${target_file}"
-    fi
-  fi
-  return "${SUCCESS}"
-}
-
-function create_directory() {
-  local directory=$1
-  echo -e "\nCreating directory ${directory}:"
-  mkdir -p "${directory}" 2>> "${DOTFILES_LOG}"
-  if [[ "$?" -ne 0 ]] ; then
-    echo '. Error on creating directory.'
-    echo -e ". Error on creating directory.\n" >> "${DOTFILES_LOG}"
-  else
-    echo '. Directory successfully created.'
-  fi
-}
-
-echo -e "\n[DOTFILES INSTALLATION] Started at $(date)\n" >> "${DOTFILES_LOG}"
-
-echo '# Dotfiles Installation #'
+echo -e "# Dotfiles Installation #\n"
 
 create_symbolic_link "${HOME}/.dotfiles/bash/bash_aliases" \
   "${HOME}/.bash_aliases"
@@ -95,6 +20,7 @@ create_directory "${HOME}/.vim/.undo"
 create_directory "${HOME}/.vim/.backup"
 create_directory "${HOME}/.vim/.swap"
 
-echo '# Finish #'
+source_file_if_linux "${HOME}/.dotfiles/install/linux.sh"
+source_file_if_macos "${HOME}/.dotfiles/install/macos.sh"
 
-echo -e "\n[DOTFILES INSTALLATION] Ended at $(date)\n" >> "${DOTFILES_LOG}"
+echo -e "\n# Finish #"
---
+++ install/functions.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Import functions
+source "${HOME}/.dotfiles/bash/functions.sh"
+
+create_symbolic_link() {
+  local target_file=$1
+  local symlink_destination=$2
+
+  if ln -sf "${target_file}" "${symlink_destination}"
+  then
+    echo "=> Symbolic link created: ${target_file} -> ${symlink_destination}"
+  fi
+}
+
+create_directory() {
+  local directory=$1
+
+  if [ -d "${directory}" ]
+  then
+    echo "=> Directory already exists: ${directory}"
+    return 0
+  fi
+
+  if mkdir -p "${directory}"
+  then
+    echo "=> Directory created: ${directory}"
+  fi
+}
+
+source_file_if_linux() {
+  local -r file_name="$1"
+
+  if [ "$(uname -s)" = "Linux" ]
+  then
+    source_file "${file_name}"
+  fi
+}
+
+source_file_if_macos() {
+  local -r file_name="$1"
+
+  if [ "$(uname -s)" = "Darwin" ]
+  then
+    source_file "${file_name}"
+  fi
+}
