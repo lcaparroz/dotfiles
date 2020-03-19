9a046bad08d9922f84d306340a737aeacad66376
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-03-19 19:24:36 -0300

    Rewrite install.sh w/ new symlink function

--- install.sh
+++ install.sh
@@ -5,31 +5,22 @@ source "${HOME}/.dotfiles/install/functions.sh"
 
 echo -e "# Dotfiles Installation #\n"
 
-create_directory "${HOME}/.emacs.d"
 create_directory "${HOME}/.config/kitty"
+create_directory "${HOME}/.emacs.d"
 create_directory "${HOME}/.vim/.backup"
 create_directory "${HOME}/.vim/.swap"
 create_directory "${HOME}/.vim/.undo"
 create_directory "${HOME}/.vim/after"
 
-create_symbolic_link "${HOME}/.dotfiles/bash/bash_aliases" \
-  "${HOME}/.bash_aliases"
-create_symbolic_link "${HOME}/.dotfiles/bash/bashrc" \
-  "${HOME}/.bashrc"
-create_symbolic_link "${HOME}/.dotfiles/config/tigrc" \
-  "${HOME}/.tigrc"
-create_symbolic_link "${HOME}/.dotfiles/emacs/init.el" \
-  "${HOME}/.emacs.d/init.el"
-create_symbolic_link "${HOME}/.dotfiles/kitty/kitty.conf" \
-  "${HOME}/.config/kitty/kitty.conf"
-create_symbolic_link "${HOME}/.dotfiles/tmux/tmux.conf" \
-  "${HOME}/.tmux.conf"
-create_symbolic_link "${HOME}/.dotfiles/vim/after/ftplugin" \
-  "${HOME}/.vim/after/ftplugin"
-create_symbolic_link "${HOME}/.dotfiles/vim/vimrc" \
-  "${HOME}/.vimrc"
-create_symbolic_link "${HOME}/.dotfiles/vim/plugins.vim" \
-  "${HOME}/.vim/plugins.vim"
+create_dotfile_symlink "bash/bash_aliases" "${HOME}/.bash_aliases"
+create_dotfile_symlink "bash/bashrc" "${HOME}/.bashrc"
+create_dotfile_symlink "config/tigrc" "${HOME}/.tigrc"
+create_dotfile_symlink "emacs/init.el" "${HOME}/.emacs.d/init.el"
+create_dotfile_symlink "kitty/kitty.conf" "${HOME}/.config/kitty/kitty.conf"
+create_dotfile_symlink "tmux/tmux.conf" "${HOME}/.tmux.conf"
+create_dotfile_symlink "vim/after/ftplugin" "${HOME}/.vim/after/ftplugin"
+create_dotfile_symlink "vim/vimrc" "${HOME}/.vimrc"
+create_dotfile_symlink "vim/plugins.vim" "${HOME}/.vim/plugins.vim"
 
 source_file_if_linux "${HOME}/.dotfiles/install/linux.sh"
 source_file_if_macos "${HOME}/.dotfiles/install/macos.sh"
