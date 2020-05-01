910a1a26a6fe0b666ccd315beea633318bf75255
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-04-30 21:51:34 -0300

    Add openSUSE font tweaks to installation step

--- linux/opensuse/install.sh
+++ linux/opensuse/install.sh
@@ -7,3 +7,7 @@ source "${HOME}/.dotfiles/share/install/functions.sh"
 echo "=> Installing openSUSE dependencies (packages)"
 xargs sudo zypper install --no-confirm --no-recommends --download-in-heaps \
   < "${HOME}/.dotfiles/linux/opensuse/dependencies.txt"
+
+# Dotfiles
+create_dotfile_symlink "linux/opensuse/config/fonts.conf" "${HOME}/.fonts.conf"
+create_dotfile_symlink "linux/opensuse/config/gtkrc-2.0"  "${HOME}/.gtkrc-2.0"
