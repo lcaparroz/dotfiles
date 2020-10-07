21d9bdc683fdb963b37e42716e674f5966c58939
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-10-07 14:08:44 -0300

    Split openSUSE dependencies into packages/patterns
    Trying to install 'x11' as a package resulted in a error because even
    though there's a package called 'x11', it's only possible to install as
    a pattern.
    
    Additionally, i renamed the '~/.gtkrc-2.0' file to '~/.gtkrc-2.0.mine',
    since the first path is overwritten whenever any changes are made with
    'lxappearance'.

--- linux/opensuse/install.sh
+++ linux/opensuse/install.sh
@@ -3,11 +3,17 @@
 # Import functions
 source "${HOME}/.dotfiles/share/install/functions.sh"
 
-# Install dependencies
-echo "=> Installing openSUSE dependencies (packages)"
-xargs sudo zypper install --no-confirm --no-recommends --download-in-heaps \
-  < "${HOME}/.dotfiles/linux/opensuse/dependencies.txt"
+# Install patterns
+echo "=> Installing openSUSE patterns"
+xargs sudo zypper install --type pattern --no-confirm --no-recommends \
+  --download-in-heaps < "${HOME}/.dotfiles/linux/opensuse/patterns.txt"
+
+# Install packages
+echo "=> Installing openSUSE packages"
+xargs sudo zypper install --type package --no-confirm --no-recommends \
+  --download-in-heaps < "${HOME}/.dotfiles/linux/opensuse/packages.txt"
 
 # Dotfiles
 create_dotfile_symlink "linux/opensuse/config/fonts.conf" "${HOME}/.fonts.conf"
-create_dotfile_symlink "linux/opensuse/config/gtkrc-2.0"  "${HOME}/.gtkrc-2.0"
+create_dotfile_symlink "linux/opensuse/config/gtkrc-2.0" \
+  "${HOME}/.gtkrc-2.0.mine"
