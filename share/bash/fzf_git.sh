e88ea2c07121353f376f64ae62902bf6719f6174
Luis Caparroz <lcaparroz.dev@gmail.com>
2021-10-27 19:12:31 -0300

    Add system color to fzf/git bash keybindings

--- share/bash/fzf_git.sh
+++ share/bash/fzf_git.sh
@@ -14,10 +14,10 @@ _fzf_git_status() {
 	_is_in_git_repo || return
 
 	git -c color.status=always status --untracked-files=all --short \
-		| _fzf_tmux -m --ansi --nth 2..,.. \
-		--preview '(git diff -- {-1} | delta --hunk-header-style="line-number syntax")' \
-		| cut -c4- \
-		| sed 's/.* -> //'
+	| _fzf_tmux -m --ansi --nth 2..,.. \
+	--preview '(git diff -- {-1} | delta --hunk-header-style="line-number syntax" --features="decorations $SYSTEM_THEME")' \
+	| cut -c4- \
+	| sed 's/.* -> //'
 }
 
 # git branch
@@ -25,26 +25,25 @@ _fzf_git_branch() {
 	_is_in_git_repo || return
 
 	git branch -a --color=always \
-		| grep -v '/HEAD\s' \
-		| sort \
-		| _fzf_tmux --ansi --multi --tac \
-		--preview 'git log --oneline --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' \
-		| sed 's/^..//' \
-		| cut -d' ' -f1 \
-		| sed 's#^remotes/##'
+	| grep -v '/HEAD\s' \
+	| sort \
+	| _fzf_tmux --ansi --multi --tac \
+	--preview 'git log --oneline --date=short --color=always --pretty="format:%C(green)%cd %C(auto)%h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' \
+	| sed 's/^..//' \
+	| cut -d' ' -f1 \
+	| sed 's#^remotes/##'
 }
 
 # git log
 _fzf_git_log() {
 	_is_in_git_repo || return
 
-	git log --date=short \
+	git log --date=short --color=always \
 	--format="%C(green)%cd %<(14,trunc)%C(magenta)%an%C(reset) %C(auto)%h%d %s" \
-	--color=always \
-		| _fzf_tmux --ansi --no-sort --multi --bind 'ctrl-s:toggle-sort' \
-		--header 'Press CTRL-S to toggle sort' \
-		--preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs -I%% bash -c "git show --combined-all-paths -c --color=always %% | delta"' \
-		| grep -o "[a-f0-9]\{7,\}"
+	| _fzf_tmux --ansi --no-sort --multi --bind 'ctrl-s:toggle-sort' \
+	--header 'Press CTRL-S to toggle sort' \
+	--preview 'git show --combined-all-paths -c --color=always $(grep -o "[a-f0-9]\{7,\}" <<< {}) | delta --features="decorations $SYSTEM_THEME"' \
+	| grep -o "[a-f0-9]\{7,\}"
 }
 
 # git stash
@@ -52,6 +51,6 @@ _fzf_git_stash() {
 	_is_in_git_repo || return
 
 	git stash list \
-		| _fzf_tmux --preview 'git stash show -p $(grep -o "^stash@{[0-9]+}" <<< {}) | delta' \
-		| cut -d: -f1
+	| _fzf_tmux --preview 'git stash show -p $(grep -oE "^stash@{[0-9]+}" <<< {}) | delta --features="decorations $SYSTEM_THEME"' \
+	| cut -d: -f1
 }
