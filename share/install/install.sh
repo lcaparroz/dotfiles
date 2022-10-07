2135c8924c1e0e582b850b5b9455dc8d1a14fb18
Luis Caparroz <lcaparroz.dev@gmail.com>
2022-10-07 15:51:12 +0200

    Add installation step to link custom scripts

--- share/install/install.sh
+++ share/install/install.sh
@@ -12,6 +12,10 @@ create_directory "$HOME/.vim/.backup"
 create_directory "$HOME/.vim/.swap"
 create_directory "$HOME/.vim/.undo"
 create_directory "$HOME/.vim/after"
+create_directory "$HOME/workspace"
+
+readonly CUSTOM_BIN="$HOME/opt/bin"
+create_directory "$CUSTOM_BIN"
 
 create_dotfile_symlink "share/bash/bash_profile" "$HOME/.bash_profile"
 create_dotfile_symlink "share/bash/bashrc" "$HOME/.bashrc"
@@ -29,6 +33,12 @@ create_dotfile_symlink "share/tmux/sessions" "$HOME/.tmux/sessions"
 create_dotfile_symlink "share/vim/after/ftplugin" "$HOME/.vim/after/ftplugin"
 create_dotfile_symlink "share/vim/vimrc" "$HOME/.vimrc"
 create_dotfile_symlink "share/vim/plugins.vim" "$HOME/.vim/plugins.vim"
+
+# Link custom scripts
+for f in ~/.dotfiles/share/bin/*
+do
+  create_symbolic_link "$f" "$CUSTOM_BIN"
+done
 
 source_file_if_linux "$HOME/.dotfiles/linux/share/install.sh"
 source_file_if_macos "$HOME/.dotfiles/macos/install.sh"
